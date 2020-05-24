b4106b92f600ed1b88b191c46b6df2da48e31d0a
Kod Planet <65843836+KodPlanet@users.noreply.github.com>
2020-05-24 14:17:03 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      title: 'Kart Postal',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xFFD7E0FF), //Colors.blueGrey[100], //#D7E0FF
